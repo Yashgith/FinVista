2f15afb559508ab0227389e4040a2f3803eebe6d
yash <yashshelar1122@gmail.com>
2024-02-24 22:12:24 +0530

    changes

--- ExpenseTracker/Backend/Config.js
+++ ExpenseTracker/Backend/Config.js
@@ -2,5 +2,5 @@ module.exports = {
     secretKey: 'secretKey',
     PORT: 3000,
     DB_URL: 'mongodb://localhost:27017/ExpenseTracker',
-    CLIENT_URL: 'https://fin-vista-client.vercel.app'
+    CLIENT_URL: 'https://fin-vista-frontend.vercel.app'
 }
